d1c8b3de009165774756befe06fd22e7636755ac
Stephan Moeller <stephanryer@hotmail.com>
2025-09-11 21:40:48 +0200

    latest files

---
+++ .zig-cache/o/ca18eb92b532125ef3a71c9044e37461/cimport.h.d
@@ -0,0 +1,11 @@
+cimport.o: \
+  /home/stephan/projects/microzig_usb_helper/.zig-cache/o/ca18eb92b532125ef3a71c9044e37461/cimport.h \
+  /home/stephan/.cache/zig/p/sqlite-3.47.2-AAAAACdIpQC782FmpW8DylXC9FX3DNs91fjcoffHjlfZ/c/sqlite3.h \
+  /usr/lib/zig/include/stdarg.h \
+  /usr/lib/zig/include/__stdarg_header_macro.h \
+  /usr/lib/zig/include/__stdarg___gnuc_va_list.h \
+  /usr/lib/zig/include/__stdarg_va_list.h \
+  /usr/lib/zig/include/__stdarg_va_arg.h \
+  /usr/lib/zig/include/__stdarg___va_copy.h \
+  /usr/lib/zig/include/__stdarg_va_copy.h \
+  /home/stephan/.cache/zig/p/sqlite-3.47.2-AAAAACdIpQC782FmpW8DylXC9FX3DNs91fjcoffHjlfZ/c/workaround.h
