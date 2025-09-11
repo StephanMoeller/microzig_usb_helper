587a7b89d92403e9510a6ad35e6d31528be8e826
Stephan Moeller <stephanryer@hotmail.com>
2025-09-11 21:45:35 +0200

    latest

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
 const microzig = @import("microzig");
 const rp2xxx = microzig.hal;
-
-const usb_if = @import("usb_if.zig");
-const usb_dev = rp2xxx.usb.Usb(.{});
-const time = rp2xxx.time;
-
 const usb_keyboard = @import("usb_keyboard.zig");
 
-var data: [7]u8 = [7]u8{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 pub fn main() !void {
     var keyboard = usb_keyboard.HelperType(.{ .max_buffer_size = 2000 }).Create();
     while (true) {
